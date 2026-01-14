5deaae57194b2f6fa28372a64f44f80aec300114
Antonio Aloisio <gnuton@gnuton.org>
2026-01-14 13:57:58 +0100

    chore: add COOP headers to vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/CardCraftStudio/',
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+    },
+  },
 })
