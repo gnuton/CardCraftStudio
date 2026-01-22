0091de47486352a4ca106e33e10c57cc6cbf3472
Antonio Aloisio <gnuton@gnuton.org>
2026-01-22 23:32:25 +0100

    fix(backend): bind to 0.0.0.0 and remove docker healthcheck

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -6,6 +6,6 @@ dotenv.config();
 const app = createApp();
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
